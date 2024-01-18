daadca2e557642527c79fb67686ea35f5ac977f1
Foxxy007 <58891333+Foxxy007@users.noreply.github.com>
2024-01-17 18:34:15 -0600

    Drive & Flywheel setted up.
    Driving Code is created.
    Flywheel Code is created.
    Corresponding Dependencies are installed.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,41 +4,66 @@
 
 package frc.robot;
 
-import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.math.proto.Controller;
+//Unused imports
+import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-
-/**
- * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
- * arcade steering.
- */
+import edu.wpi.first.wpilibj.Joystick;
+// Utility class
+import frc.robot.Utility;
 public class Robot extends TimedRobot {
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
-  private final DifferentialDrive m_robotDrive =
-      new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
-  private final Joystick m_stick = new Joystick(0);
-
+  // Drive motors
+  WPI_VictorSPX motorLeftFront = new WPI_VictorSPX(1);
+  WPI_VictorSPX motorLeftBack = new WPI_VictorSPX(2);
+  WPI_VictorSPX motorRightFront = new WPI_VictorSPX(3);
+  WPI_VictorSPX motorRightBack = new WPI_VictorSPX(4);
+  final GenericHID Controller1 = new GenericHID(0);
+  // Movement
+  double turn;
+  double drive;
+  // Flywheel
+  CANSparkMax flywheelFront = new CANSparkMax(11, MotorType.kBrushed);
+  CANSparkMax flywheelBack = new CANSparkMax(10, MotorType.kBrushed);
+  double speed;
+  boolean a;
   public Robot() {
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
+    
   }
-
+  
   @Override
   public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+
   }
 
   @Override
   public void teleopPeriodic() {
-    // Drive with arcade drive.
-    // That means that the Y axis drives forward
-    // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(-m_stick.getY(), -m_stick.getX());
+    //Re-defining
+    turn = Controller1.getRawAxis(0);//x-axis 1
+    drive = Controller1.getRawAxis(1);//y-axis 1
+
+    speed = Controller1.getRawAxis(3);//x-axis 2
+    
+    a = Controller1.getRawButton(1);
+    //Drive
+    motorLeftFront.set(drive+turn);
+    motorLeftBack.set(drive+turn);
+    motorRightFront.set(-drive+turn);
+    motorRightBack.set(-drive+turn);
+    //Flywheel
+    flywheelFront.set(speed);
+    if(a==true){
+      flywheelBack.set(speed);
+    }else{
+      flywheelBack.set(0);
+    }
   }
-}
+
+  }
+

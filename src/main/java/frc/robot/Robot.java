7e4fd59eb14e684a27c07035e272db97fb43c9c0
AidenSeiler <119818540+AidenSeiler@users.noreply.github.com>
2024-01-19 19:56:28 -0600

    Basic Limelight Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,32 +5,18 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.math.proto.Controller;
-//Unused imports
-import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.Joystick;
-// Utility class
-import frc.robot.Utility;
-//Controller Binds
-/* Axis 0 - X Stick 1
-Axis 1 - Y Stick 1(Not Spring-Loaded)
-Axis 2 - X Stick 2
-Axis 3 - Y Stick 2
-Axis 4 - Left Slider(Potentiometer)
-Axis 5 - Right Slider(Potentiometer)
-Axis 6 - Switch E(three way top left)
-Axis 7 - Switch F(three way top left)
-Button 1,2,3 - Switch B(three way front left)
-Button 4,5,6 - Switch C(three way front right)
-Button 7 - Button a
-Button 8 - Button D */
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 public class Robot extends TimedRobot {
   // Drive motors
@@ -39,44 +25,54 @@ public class Robot extends TimedRobot {
   WPI_VictorSPX motorRightFront = new WPI_VictorSPX(3);
   WPI_VictorSPX motorRightBack = new WPI_VictorSPX(4);
   final GenericHID Controller1 = new GenericHID(0);
-  // Movement
   double turn;
   double drive;
   // Flywheel
   CANSparkMax flywheelFront = new CANSparkMax(11, MotorType.kBrushed);
   CANSparkMax flywheelBack = new CANSparkMax(10, MotorType.kBrushed);
-  double speed;
+  double flywheelSpeed;
   boolean a;
-  public Robot() {
-    
+  //Current Sensing
+  PowerDistribution powerPanel = new PowerDistribution(1, ModuleType.kRev);
+
+  public Robot() { 
   }
   
   @Override
   public void robotInit() {
-
   }
 
   @Override
   public void teleopPeriodic() {
-    //Re-defining
+    //LimeLight Variable Updates
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
+    double rawX = tx.getDouble(0.0);
+    double rawY = ty.getDouble(0.0);
+    double rawArea = ta.getDouble(0.0);
+    //Robot actions
     turn = Controller1.getRawAxis(0);//x-axis 1
     drive = Controller1.getRawAxis(3);//y-axis 2
-
-    speed = Controller1.getRawAxis(4);//x-axis 2
-    
-    a = Controller1.getRawButton(7);
+    flywheelSpeed = Controller1.getRawAxis(4);//x-axis 2
+    a = Controller1.getRawButton(7);//a-button
     //Drive
     motorLeftFront.set(drive+turn);
     motorLeftBack.set(drive+turn);
     motorRightFront.set(-drive+turn);
     motorRightBack.set(-drive+turn);
     //Flywheel
-    flywheelFront.set(speed);
-    if(a==true){
-      flywheelBack.set(speed);
+    flywheelFront.set(flywheelSpeed);
+    if(a){
+      flywheelBack.set(flywheelSpeed);
     }else{
       flywheelBack.set(0);
     }
+    //Telemetry
+    SmartDashboard.putNumber("LimelightX", rawX);
+    SmartDashboard.putNumber("LimelightY", rawY);
+    SmartDashboard.putNumber("LimelightArea", rawArea);
   }
 
   }

5b04079e610e354271f37a0d49d69b571e39c602
AidenSeiler <119818540+AidenSeiler@users.noreply.github.com>
2024-02-16 15:40:05 -0600

    Command based branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,18 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
-
-public class Constants {
-    public static final double ksVolts = 0;
-    public static final double kvVoltSecondsPerMeter = 0;
-    public static final double kaVoltSecondsSquaredPerMeter = 0;
-    public static final double kPDriveVel = 0;
-    public static final double kTrackwidthMeters = 0;
-    public static final DifferentialDriveKinematics kDriveKinematics =
-    new DifferentialDriveKinematics(kTrackwidthMeters);
-    public static final double kMaxSpeedMetersPerSecond = 0;
-    public static final double kMaxAccelerationMetersPerSecondSquared = 0;
-    public static final double kRamseteB = 0;
-    public static final double kRamseteZeta = 0;
-    public static final double kEncoderDistancePerPulse = 0; //I don't know if this is per pulse or per rotation
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,179 +4,100 @@
 
 package frc.robot;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkRelativeEncoder;
-
-import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.PowerDistribution;
-import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
-  AHRS ahrs;
-  // Drive motors
-  TalonFX motorLeft = new TalonFX(2);
-  TalonFX motorLeftFollower = new TalonFX(3);
-  TalonFX motorRight = new TalonFX(1);
-  TalonFX motorRightFollower = new TalonFX(4);
+  private Command m_autonomousCommand;
 
-  // Create Controller
-  GenericHID Controller1 = new GenericHID(0);
-  double turn;
-  double drive;
+  private RobotContainer m_robotContainer;
 
-  // Flywheel & Intake
-  CANSparkMax flywheelLeftFront = new CANSparkMax(6, MotorType.kBrushless);
-  CANSparkMax flywheelLeftBack = new CANSparkMax(7, MotorType.kBrushless);
-  CANSparkMax flywheelRightFront = new CANSparkMax(5, MotorType.kBrushless);
-  CANSparkMax flywheelRightBack = new CANSparkMax(8, MotorType.kBrushless);
-  CANSparkMax intakeBelt = new CANSparkMax(3, MotorType.kBrushless);
-  CANSparkMax intakeTopRoller = new CANSparkMax(4, MotorType.kBrushed);
-  CANSparkMax intakeBottomRoller = new CANSparkMax(2, MotorType.kBrushed);
-  CANSparkMax frontMotor = new CANSparkMax(10, MotorType.kBrushless);
-
-  //LED
-  Spark blinkin = new Spark(0);
-  ShuffleboardTab tab = Shuffleboard.getTab("LED");
-  GenericEntry ledPWM = tab.add("Max Speed", 1).getEntry();
-  double idleLED = -0.21;
-  double speakerLED = -0.97;
-  double ampLED = -0.92; 
-  double stickX2;
-  double stickY1;
-  double stickY2;
-  boolean buttonD;
-  boolean buttonA;
-  boolean buttonG;
-
-
-
-  //Current Sensing
-  PowerDistribution powerPanel = new PowerDistribution(1, ModuleType.kRev);
-  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-
-  public Robot() { 
-  }
-  
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
   @Override
   public void robotInit() {
-    frontMotor.setInverted(true);
-    flywheelRightFront.setInverted(true);
-    flywheelRightBack.setInverted(true);
-    intakeTopRoller.setInverted(true);
-    intakeBottomRoller.setInverted(true);
-
-    motorLeftFollower.setControl(new Follower(motorLeft.getDeviceID(), false));
-    motorRightFollower.setControl(new Follower(motorRight.getDeviceID(), false));
-
-    ahrs = new AHRS(SPI.Port.kMXP);
-    
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
   }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
   @Override
-  public void autonomousPeriodic() {
-    //Autonomous
-    //RamseteController controller1 = new RamseteController();
-    
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
   }
+
+  /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void teleopPeriodic() {
-    if (ahrs.isCalibrating()) {
-      //Thread.yield();
-      SmartDashboard.putNumber("Is Calibrating", 1 );
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
     }
-    if(ahrs.isConnected()){
-      SmartDashboard.putNumber("Is Connected", 1 );
-    }
-
-    //LimeLight Variable Updates
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry ta = table.getEntry("ta");
-    double rawX = tx.getDouble(0.0);
-    double rawY = ty.getDouble(0.0);
-    double rawArea = ta.getDouble(0.0);
-
-    //Controller Mapping
-    stickX2 = Controller1.getRawAxis(3);
-    stickY1 = Controller1.getRawAxis(1);
-    stickY2 = Controller1.getRawAxis(4);
-    buttonA = Controller1.getRawButton(3);
-    buttonD = Controller1.getRawButton(4);
-    buttonG = Controller1.getRawButton(5);
-
-    //Robot actions
-    turn = Util.clamp(Util.inputCurve(stickX2,0.2));//x-axis 1
-    drive = Util.clamp(-Util.inputCurve(stickY1, 0.2));//y-axis 2
-
-    //LED
-    //double led = ledPWM.getDouble(1.0);
-    //blinkin.set(led);
-    //Drive
-    motorLeft.set(drive+turn);
-    motorRight.set(-drive+turn);
-    
-
-    //Flywheel and Intake
-    intakeBelt.set(stickY2);
-    intakeTopRoller.set(stickY2);
-    intakeBottomRoller.set(stickY2);
-
-    if(buttonD){ //AmpScoring
-      frontMotor.set(0.1);
-      flywheelLeftBack.set(0.20);
-      flywheelRightBack.set(0.20);
-      flywheelLeftFront.set(0.1);
-      flywheelRightFront.set(0.1);
-      blinkin.set(ampLED);
-    }
-    else if(buttonA){ //Speaker Scoring
-      flywheelLeftBack.set(1);
-      flywheelRightBack.set(1);
-      flywheelLeftFront.set(1);
-      flywheelRightFront.set(1);
-      blinkin.set(speakerLED);
-    }else if(buttonG){//Intake through shooter
-      flywheelLeftBack.set(-0.15);
-      flywheelRightBack.set(-0.15);
-      flywheelLeftFront.set(-0.15);
-      flywheelRightFront.set(-0.15);
-    }else{
-      frontMotor.set(0);
-      flywheelLeftBack.set(0);
-      flywheelRightBack.set(0);
-      flywheelLeftFront.set(0);
-      flywheelRightFront.set(0);
-      blinkin.set(idleLED);
-    } 
-    
-    //Telemetry
-    SmartDashboard.putNumber("Limelight X", rawX);
-    SmartDashboard.putNumber("Limelight Y", rawY);
-    SmartDashboard.putNumber("Limelight Area", rawArea);
-    SmartDashboard.putNumber("Total Current", powerPanel.getTotalCurrent());
-    SmartDashboard.putNumber("Test Angle",ahrs.getYaw());
-    //SmartDashboard.putNumber("Front Flywheel Speed",leftSlider);
-    //SmartDashboard.putNumber("Rear Flywheel Speed",rightSlider);
-
-
-
   }
 
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
   }
 
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}

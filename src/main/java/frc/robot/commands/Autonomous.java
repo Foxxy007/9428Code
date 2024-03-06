28252d7d1c49a3fbea85c8788a110c4cdb72d6d4
greentractor202 <119818540+AidenSeiler@users.noreply.github.com>
2024-03-06 11:51:36 -0600

    Fixed and finished DrivewithJoysticks

--- src/main/java/frc/robot/commands/Autonomous.java
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -30,7 +30,7 @@ public class Autonomous extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute(){
-   // m_AutoDrivetrain.autoDrive();
+    m_AutoDrivetrain.autoDrive();
   }
 
   // Called once the command ends or is interrupted.
